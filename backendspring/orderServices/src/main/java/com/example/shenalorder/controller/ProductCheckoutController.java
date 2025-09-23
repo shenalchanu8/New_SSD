9ffe9798d5aee70ae94e17c3373a21a7dd7d8b67
shenalchanu8 <125878126+shenalchanu8@users.noreply.github.com>
2025-09-23 21:53:39 +0530

    update productpage

--- backendspring/orderServices/src/main/java/com/example/shenalorder/controller/ProductCheckoutController.java
+++ backendspring/orderServices/src/main/java/com/example/shenalorder/controller/ProductCheckoutController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/product/v1")
 public class ProductCheckoutController {
 
