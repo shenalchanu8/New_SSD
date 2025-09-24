6fb1a56dc2fa188876ca885a04c5bcc50342ce62
shenalchanu8 <125878126+shenalchanu8@users.noreply.github.com>
2025-09-24 16:29:31 +0530

    Update Delivery Controller - Cross Origin

--- backendspring/deliverySerice/src/main/java/com/example/deliverKavishka/controller/DeliveryController.java
+++ backendspring/deliverySerice/src/main/java/com/example/deliverKavishka/controller/DeliveryController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/deliveries")
 public class DeliveryController {
 
