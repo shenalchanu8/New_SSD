2d3cfc0dfd0ba8891c599ffa901482f0f9b3c42b
Kavishka Senavirathna <kavishkasenavirathna6@gmail.com>
2025-09-24 17:16:01 +0530

    Update CORS configuration in controllers to restrict origins to http://localhost:3000

--- backendspring/orderServices/src/main/java/com/example/shenalorder/controller/OrderController.java
+++ backendspring/orderServices/src/main/java/com/example/shenalorder/controller/OrderController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/orders")
 public class OrderController {
 
