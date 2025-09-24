8fd9ddc5ac4f6704e37f27e05a3353b903dbbde4
shenalchanu8 <125878126+shenalchanu8@users.noreply.github.com>
2025-09-24 15:36:21 +0530

    Revert "update crose"
    This reverts commit c88781996e59af6a936528d731acda32ebc2126f.

--- backendspring/userServices/src/main/java/com/nexests/nexests/controller/AuthController.java
+++ backendspring/userServices/src/main/java/com/nexests/nexests/controller/AuthController.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/auth")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 
 public class AuthController {
     @Autowired
